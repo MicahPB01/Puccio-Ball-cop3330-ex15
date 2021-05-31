fc4a1ce0221dd00de977355f819661f5594b49b9
MicahPB01 <micahpuccioball@gmail.com>
2021-05-31 15:33:35 -0400

    Initial commit

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,40 @@
+package oop.example;
+
+import java.util.*;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        String userInput;
+
+        ArrayList<String> listPass = new ArrayList<String>();
+
+        listPass.add("Hello123");
+        listPass.add("PASSWORD");
+        listPass.add("abc$123");
+        listPass.add("Testing156");
+
+        System.out.printf("What is the password? ");
+        userInput = input.nextLine();
+
+        if (listPass.contains(userInput)) {
+            System.out.printf("Welcome!\n");
+        }
+        else   {
+            System.out.printf("I don't know you.\n");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
